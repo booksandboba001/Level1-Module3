fa236a49aa2b997248656734ffb42380e4dfee2a
653955 <653955@SDUSD-MZ01PR8A.sdcs.local>
2025-11-05 16:48:41 -0800

    finished froggerrrrr

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -1,5 +1,7 @@
 package _06_frogger;
 
+import java.awt.Graphics;
+
 import processing.core.PApplet;
 
 public class Frogger extends PApplet {
@@ -8,6 +10,9 @@ public class Frogger extends PApplet {
     static int distance;
     static int x =300;
     static int y= 389;
+    Car car1 = new Car(100,50, 500,50);
+    Car car2 = new Car(60,150, 500,50);
+    Car car3 = new Car(60,250, 500,50);
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
@@ -23,8 +28,27 @@ public class Frogger extends PApplet {
     	background(255,255,255);
     	fill(0,255,0);
     	ellipse(x,y,20,20);
-    	 
+    	car1.display(g);
+    	car1.moveLeft(15);
+    	car2.display(g);
+    	car2.moveRight(10);
+    	if (intersects(car2)) {
+    		y=389;
+    	}
+    	car3.display(g);
+    	car3.moveRight(10);
+    	if (intersects(car3)) {
+    		y=389;
+    	}
+    	
     }
+    boolean intersects(Car car) { 
+		if ((y > car.getY() && y < car.getY()+50) &&(x > car.getX() && x < car.getX()+car.getSize())) {
+			return true;
+			
+		}else{return false;
+			}
+	}
     public void keyPressed() {
     	if(key == CODED){
     		if(keyCode == UP){
